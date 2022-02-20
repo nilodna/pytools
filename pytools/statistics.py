e836a71853a81abcf97b12bf485e8c0df8960002
Danilo A. Silva <nilodna@gmail>
2022-02-20 11:06:42 -0300

    inserting statistical functions to non-parametric analysis and determination of confidence interval. Functions used in Silta et al. (in prep)

--- pytools/statistics.py
+++ pytools/statistics.py
@@ -1,10 +1,13 @@
 """
-	. Nonte: some functions (principal_ang, Tdecorr, Neff) were borrowed from https://github.com/apaloczy/ap_tools
+	. Note: some functions (principal_ang, Tdecorr, Neff) were borrowed from https://github.com/apaloczy/ap_tools
  
  
 """
 import numpy as np
+import scipy.stats as stats
+import math
 
+#########################################################################################################
 
 
 def autocorr(x, biased=True):
@@ -64,7 +67,8 @@ def autocorr(x, biased=True):
 	return Rxx
     
 #########################################################################################################
-    
+
+
 def principal_ang(x, y):
 	"""
 	USAGE
@@ -92,6 +96,7 @@ def principal_ang(x, y):
 	return th_princ
 
 #########################################################################################################
+
 
 def Tdecorr(Rxx, M=None, dtau=1., verbose=False):
     """
@@ -156,6 +161,7 @@ def Tdecorr(Rxx, M=None, dtau=1., verbose=False):
 
 #########################################################################################################
 
+
 def Neff(Tdecorr, N, dt=1.):
 	"""
 	USAGE
@@ -180,3 +186,206 @@ def Neff(Tdecorr, N, dt=1.):
 
 #########################################################################################################
 
+
+def medianCI(sample, confidence=0.95):
+    """Get the confidence interval for the median of a sample
+
+    Based on the work of Campbell and Gardner (1988), the confidence interval is
+    calculated by the following equation:
+
+        r = n/2 - N * np.sqrt(n)/2
+
+        s = 1 + n/2 + N * np.sqrt(n)/2
+
+    Parameters
+    ----------
+    sample : numpy.ndarray, list
+        sample array
+    confidence : float, optional
+        Confidence to compute the interval, by default 0.95
+
+    Returns
+    -------
+    [type]
+        [description]
+        
+    References
+    ----------
+    
+    """
+    # method based on Campbell and Gardner (1988)
+    
+    # find N = (1-alpha/2)
+    alpha = 1 - confidence
+    N = stats.norm.ppf(1 - alpha/2)
+    
+    # sample size (n)
+    n = len(sample)
+    
+    # compute the position of the lower limit value
+    r = n/2 - N * (np.sqrt(n)/2)
+    r = round(r)
+    
+    # compute the position of the upper limit value
+    s = 1 + n/2 + N * (np.sqrt(n)/2)
+    s = round(s)
+    
+    # construct CI tuple
+    CI = (sample[r], sample[s])
+    
+    return CI, N * (np.sqrt(n)/2)
+
+#########################################################################################################
+
+
+def ci_for_proportion(df, var1, var2, group, conf=.95, method=None):
+    """Function to calculate the confidence interval for a very specific dataframe.
+    
+    If the method chosen is 'clopper_pearson', then compute the confidence interval by:
+        [[equation]]
+    Otherwise, compute the confidence interval with a general equation:
+        [[equation]]
+    
+    based on: https://towardsdatascience.com/a-complete-guide-to-confidence-interval-and-examples-in-python-ff417c5cb593
+    """       
+    # get z-score based on the confidence level given (conf)
+    alpha = 1 - conf
+    zscore = stats.norm.ppf(1-alpha/2)
+    
+    # create contingency table (2x2)
+    df_crosstab = pd.crosstab(df[var1], df[var2]).T
+    
+    N = df_crosstab[group].sum()
+    s = df_crosstab[group][1] # s of success
+    
+    percentage = s / N
+    standard_error = np.sqrt(percentage * (1 - percentage)/N)
+    
+    if method == 'clopper_pearson':
+        b = stats.beta.ppf
+        lo = b(alpha / 2, s, N - s + 1)
+        hi = b(1 - alpha / 2, s + 1, N - s)
+        
+        data = [percentage,                    # percentage
+                0.0 if math.isnan(lo) else lo, # lower CI
+                1.0 if math.isnan(hi) else hi] # upper CI
+    else:
+        # apply a broader method
+        # if N < 30, then the sample must have a normal distribution. Otherwise, a normal distribution is not necessary
+        if N < 30:
+            print('Sample size lower than 30, therefore a normal distribution is requested. \nIf your sample doens\'t have a normal distribution, be careful with these results.')
+
+        ci = zscore*standard_error
+
+        lower_ci = percentage - ci
+        upper_ci = percentage + ci
+        
+        data = [percentage,
+                lower_ci,
+                upper_ci,
+                standard_error,
+                ci            
+        ]
+
+    return data
+
+#########################################################################################################
+
+
+def non_param_unpaired_CI(sample1, sample2, conf):
+    n1 = len(sample1)  
+    n2 = len(sample2)  
+    alpha = 1-conf      
+    N = stats.norm.ppf(1 - alpha/2) 
+
+    # The confidence interval for the difference between the two population
+    # medians is derived through the n x m differences.
+    diffs = sorted([i-j for i in sample1 for j in sample2])
+
+    # the Kth smallest to the Kth largest of the n x m differences then determine 
+    # the confidence interval, where K is:
+    k = np.math.ceil(n1*n2/2 - (N * (n1*n2*(n1+n2+1)/12)**0.5))
+
+    CI = (round(diffs[k-1],3), round(diffs[len(diffs)-k],3))
+    return CI
+
+#########################################################################################################
+
+
+def spec_error(E,sn,ci=.95):
+    """ 
+    Computes confidence interval for one-dimensional spectral estimate E.
+
+    Parameters
+    ===========
+    - sn is the number of spectral realizations;
+            it can be either an scalar or an array of size(E)
+    - ci = .95 for 95 % confidence interval
+
+    Output
+    ==========
+    lower (El) and upper (Eu) bounds on E 
+    
+    Usage
+    ======
+    >> data = 
+    >> f, ddof, spec = block_avgz(b1s, 1/freq/60, 12)
+    >> spec_lower, spec_upper = spec_error(spec, ddof/2, ci=0.95)
+    >> plt.plot(spec)
+    >> plt.fill_between(f, spec_lower, y2=spec_upper, alpha=.2)
+    
+    Source
+    =======
+    https://nbviewer.org/github/apaloczy/InnerShelfReynoldsStresses/blob/master/plot_figs/fig02/fig02.ipynb
+    """
+
+    from pyspec.spectrum import yNlu
+
+    dbin = .005
+    yN = np.arange(0,2.+dbin,dbin)
+
+    El, Eu = np.empty_like(E), np.empty_like(E)
+
+    yNl,yNu = yNlu(sn,yN=yN,ci=ci)
+    El = E/yNl
+    Eu = E/yNu
+
+    return El, Eu
+
+############################################################################################################
+
+
+def block_avgz(A, dts, Nblks):
+    """
+    compute spectral analysis for each vertical level and then return the depth-averaged spectrum,
+    along with a degrees of freedom (dof) and frequency (f).
+    
+    """
+    from pyspec.spectrum import block_avg
+    
+    nz, nt = A.shape
+    for k in range(nz):
+        a = A[k, :]
+        fg = np.isfinite(a)
+        if not fg.any():
+            if k==0:
+                if not a.size%2:
+                    siz = a.size/2/Nblks + 1
+                else:
+                    siz = a.size/2/Nblks
+                S = np.ones(int(siz))*np.nan
+            else:
+                S = np.vstack((S, Saa.f*np.nan))
+            continue
+        a[~fg] = np.nanmean(a)
+        Saa, nblks = block_avg(a, dts, N=Nblks);
+        if k==0:
+            S = Saa.spec
+        else:
+            S = np.vstack((S, Saa.spec[np.newaxis, ...]))
+    f = Saa.f
+
+    dof = 2*nblks
+    return f, dof, np.nanmean(S, axis=0)
+
+############################################################################################################
